c72f4ab00250878b278c18790bffd46359aec78c
Shane Hickman <srhickma@edu.uwaterloo.ca>
2017-08-12 22:11:56 -0400

    refactored controllers and addes time controller

---
+++ Assets/GameAssets/Scripts/Controllers/TimeController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TimeController : MonoBehaviour {
+
+	private const float SECONDS_PER_GAME_MIN = 0.1f;
+
+	public int day{ get; private set; }
+	public int hour{ get; private set; }
+	public int min{ get; private set; }
+
+	private float elapsedSeconds;
+
+	private void Awake(){
+		day = 1;
+		hour = 0;
+		elapsedSeconds = 0f;
+	}
+
+	private void Update(){
+		elapsedSeconds += Time.deltaTime;
+		if(elapsedSeconds > SECONDS_PER_GAME_MIN){
+			min++;
+			if(min > 59){
+				min = 0;
+				hour++;
+				if(hour > 23){
+					hour = 0;
+					day++;
+				}
+			}
+			elapsedSeconds = 0f;
+		}
+	}
+
+	private string getTimeString(){
+		return string.Format("{0:D2}:{1:D2} Day {2}", hour, min, day);
+	}
+	
+}
